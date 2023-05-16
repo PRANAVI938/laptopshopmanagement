c1758489351d7ce21e5cc8686a66db585c77db42
MUDDAM PRANAVI <75021708+PRANAVI938@users.noreply.github.com>
2023-05-16 16:02:38 +0530

    Add files via upload

---
+++ com.in.laptop/src/main/java/com/in/laptop/LaptopShopManagementApplication.java
@@ -0,0 +1,13 @@
+package com.in.laptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopShopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopShopManagementApplication.class, args);
+	}
+
+}
---
+++ com.in.laptop/src/test/java/com/in/laptop/LaptopShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.laptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
